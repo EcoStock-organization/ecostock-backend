615e36341c1f6bf1c9a23e9eb37da7a7d66eea94
DiegoCarlito <diego.carlito01@gmail.com>
2025-11-23 06:54:09 -0300

    feat: adicionar comunicação com o serviço de autenticação

---
+++ src/usuario/services.py
@@ -0,0 +1,25 @@
+import requests
+from rest_framework.exceptions import APIException
+import os
+
+AUTH_SERVICE_URL = os.getenv('AUTH_SERVICE_URL', 'http://auth_backend:8000/api') 
+
+class AuthService:
+    @staticmethod
+    def criar_usuario_auth(username, email, password):
+        url = f"{AUTH_SERVICE_URL}/internal/users/"
+        
+        try:
+            response = requests.post(url, json={
+                "username": username,
+                "email": email,
+                "password": password
+            })
+            
+            if response.status_code == 201:
+                return response.json()
+            else:
+                raise APIException(detail=f"Erro no serviço de Auth: {response.text}")
+                
+        except requests.exceptions.RequestException as e:
+            raise APIException(detail=f"Falha de conexão com serviço de Auth: {str(e)}")
